0f162129d8b2fa2d4434b5fec0d48e50333f5da3
canerdikkollu <cdikkollu@gmail.com>
2018-08-15 22:32:16 +0300

    article content search

--- MvcBlog/Controllers/HomeController.cs
+++ MvcBlog/Controllers/HomeController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcBlog.Models;
-
+using PagedList;
+using PagedList.Mvc;
 
 namespace MvcBlog.Controllers
 {
@@ -13,34 +14,70 @@ namespace MvcBlog.Controllers
         mvcblogDB db = new mvcblogDB();
 
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(int Page=1)
         {
-            var makale = db.Makales.OrderByDescending(m => m.MakaleId).ToList();
+
+            var makale = db.Makales.OrderByDescending(m => m.MakaleId).ToPagedList(Page,5);
             return View(makale);
+        }
+
+        public ActionResult BlogAra(string Ara=null)
+        {
+            var aranan = db.Makales.Where(m => m.Baslik.Contains(Ara)).ToList();
+            return View(aranan.OrderByDescending(m => m.Tarih)); //tarihe göre sırala
+        }
+        public ActionResult KategoriMakale(int id)
+        {
+            var makaleler = db.Makales.Where(m => m.Kategori.KategoriId == id).ToList();
+            return View(makaleler);
         }
 
         public ActionResult MakaleDetay(int id)
         {
             var makale = db.Makales.Where(m => m.MakaleId == id).SingleOrDefault();
-            if(makale==null)
+            if (makale == null)
             {
                 return HttpNotFound();
             }
             return View(makale);
         }
 
-        public ActionResult YorumYap(string yorum,int MakaleId)
+        public ActionResult YorumYap(string yorum, int MakaleId)
         {
             var uyeid = Session["uyeid"];
-            if (yorum ==null)
+            if (yorum == null)
             {
                 return Json(true, JsonRequestBehavior.AllowGet);
             }
 
             db.Yorums.Add(new Yorum { UyeId = Convert.ToInt32(uyeid), MakaleId = MakaleId, Icerik = yorum, Tarih = DateTime.Now });
             db.SaveChanges();
-           
+
             return Json(false, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult YorumSil(int id)
+        {
+            var uyeid = Session["uyeid"];
+            var yorum = db.Yorums.Where(y => y.YorumId == id).SingleOrDefault();
+            var makale = db.Makales.Where(m => m.MakaleId == yorum.MakaleId).SingleOrDefault();
+
+            if (yorum.UyeId == Convert.ToInt32(uyeid))
+            {
+                db.Yorums.Remove(yorum);
+                db.SaveChanges();
+                return RedirectToAction("MakaleDetay", "Home", new { id = makale.MakaleId });
+            }
+            else
+                return HttpNotFound();
+        }
+
+        public ActionResult OkunmaArttir(int Makaleid)
+        {
+            var makale = db.Makales.Where(m => m.MakaleId == Makaleid).SingleOrDefault();
+            makale.Okunma += 1;
+            db.SaveChanges();
+            return View();
         }
 
         public ActionResult Hakkimizda()
@@ -57,6 +94,6 @@ namespace MvcBlog.Controllers
         {
             return View(db.Kategoris.ToList());
         }
-      
+
     }
 }
